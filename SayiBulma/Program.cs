27c50918fb3deabd1f963a7a9909abeb9a59943d
berat584 <berat584@BPC08>
2023-11-28 10:53:26 +0300

    Proje dosyası ekle.

---
+++ SayiBulma/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayiBulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayiBul sayiBul = new SayiBul();
+            double bs=sayiBul.SayiyiSec(4, 40);
+            Console.WriteLine( "Büyük Sayi:{0}",bs);
+        }
+    }
+    public class SayiBul
+    {
+        public double SayiyiSec(double sayi1, double sayi2)
+        {
+            double sonuc = 0;
+            if (sayi1 >= sayi2)
+            {
+                sonuc=sayi1;
+            }
+            else
+            {
+                sonuc=sayi2;
+            }
+            if (sayi1 == sayi2)
+            {
+                sayi1=sayi2;
+            }
+            return sonuc;
+
+        }
+    }
+}
